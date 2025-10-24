6bbc75bc742c84fef9692f2feac0c83b132a76ae
Pandemolyum <77362203+Pandemolyum@users.noreply.github.com>
2025-10-24 12:56:50 -0400

    Add task display and styling

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ import "./style.css";
 import {Task, projects, displayController} from "./objects.js";
 
 const firstTask = new Task("All Tasks");
-projects.list["All Tasks"].push(firstTask);
+projects.addTask("All Tasks", firstTask);
+const secondTask = new Task("All Tasks");
+projects.addTask("All Tasks", secondTask);
+const thirdTask = new Task("All Tasks");
+projects.addTask("All Tasks", thirdTask);
+
+displayController.displayProject("All Tasks", projects.list["All Tasks"]);
 
 console.log(projects.list);
 
--- src/objects.js
+++ src/objects.js
@@ -2,10 +2,11 @@ class Task {
     constructor(projectName) {
         this.uuid = self.crypto.randomUUID();
         this.projectName = projectName;
-        this.taskName = "";
-        this.dueDate = "";
+        this.title = "Untitled";
+        this.dueDate = "2025-01-01";
         this.priority = "Normal";
-        this.description = "";
+        this.description = "Lorem ipsum bla bla bla";
+        this.isCompleted = false;
     }
 }
 
@@ -13,18 +14,56 @@ const projects = {
     list: {"All Tasks": []}, // array to store projects and tasks
 
     addProject: function(name) {
-        return list.push(name);
-    }
+        this.list[name] = [];
+    },
+
+    addTask: function(projectName, task) {
+        this.list[projectName].push(task);
+    },
 };
 
 const displayController = {
-    displayProject: function() {
+    displayProject: function(projectName, taskArray) {
+        // Select project tab
+        const projectList = document.querySelectorAll(".project");
+        console.log(projectList)
+        projectList.forEach((elem) => {
+            if (elem.textContent === projectName) {
+                elem.className = "project active";
+            }
+        });
 
+        // Display tasks
+        const nCompDiv = document.querySelector("#notCompleted")
+        const compDiv = document.querySelector("#completed")
+        for (let task of taskArray) {
+            const taskDiv = document.createElement("div");
+            const taskCheck = document.createElement("input")
+            const taskTitle = document.createElement("h2");
+            const taskDate = document.createElement("span");
+            const taskDesc = document.createElement("p");
+
+            taskTitle.textContent = task.title;
+            taskCheck.type = "checkbox";
+            taskDate.textContent = task.dueDate;
+            taskDesc.textContent = task.description;
+
+            taskDiv.appendChild(taskCheck);
+            taskDiv.appendChild(taskTitle);
+            taskDiv.appendChild(taskDate);
+            taskDiv.appendChild(taskDesc);
+            
+            if (!task.isCompleted) {
+                nCompDiv.appendChild(taskDiv);
+            } else {
+                compDiv.appendChild(taskDiv);
+            }
+        }
     },
 
     displayTask: function() {
 
-    }
+    },
 };
 
 export {Task, projects, displayController};
