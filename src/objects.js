156cc77ea7734f10faac631273b1a3f6b1d41dee
Pandemolyum <77362203+Pandemolyum@users.noreply.github.com>
2025-10-24 14:15:28 -0400

    Add card styling based on priority

--- src/objects.js
+++ src/objects.js
@@ -29,7 +29,7 @@ const displayController = {
         console.log(projectList)
         projectList.forEach((elem) => {
             if (elem.textContent === projectName) {
-                elem.className = "project active";
+                elem.classList.add("active");
             }
         });
 
@@ -37,16 +37,20 @@ const displayController = {
         const nCompDiv = document.querySelector("#notCompleted")
         const compDiv = document.querySelector("#completed")
         for (let task of taskArray) {
-            const taskDiv = document.createElement("div");
+            const taskDiv = document.createElement("div"); // need to track this after creation and associate it to task
             const taskCheck = document.createElement("input")
             const taskTitle = document.createElement("h2");
             const taskDate = document.createElement("span");
             const taskDesc = document.createElement("p");
 
+            // Need to separate some of these into smaller functions
             taskTitle.textContent = task.title;
             taskCheck.type = "checkbox";
+            taskDate.style.justifySelf = "right";
             taskDate.textContent = task.dueDate;
             taskDesc.textContent = task.description;
+
+            this.onChangeTaskPriority(task, taskDiv);
 
             taskDiv.appendChild(taskCheck);
             taskDiv.appendChild(taskTitle);
@@ -64,6 +68,20 @@ const displayController = {
     displayTask: function() {
 
     },
+
+    onChangeTaskPriority: function(task, taskDiv) {
+        switch (task.priority) {
+            case "Low": 
+                taskDiv.classList.add("low");
+                break;
+            case "Normal": 
+                taskDiv.classList.add("normal");
+                break;
+            case "High": 
+                taskDiv.classList.add("high");
+                break;
+        }
+    }
 };
 
 export {Task, projects, displayController};
