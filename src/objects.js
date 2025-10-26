25c068df7d76a716883e9ff86d5a49123c72e89e
Pandemolyum <77362203+Pandemolyum@users.noreply.github.com>
2025-10-26 15:50:47 -0400

    Modify how tasks in project list are displayed
    Instad of creating the divs for completed and uncompleted tasks directly
    inside template.html, they are now created in objects.js display
    controller for flexibility.

--- src/objects.js
+++ src/objects.js
@@ -24,7 +24,24 @@ const projects = {
 
 const displayController = {
     displayProject: function(projectName, taskArray) {
-        // Select project tab
+        // Clear display and add new divs
+        const contentDiv = document.querySelector("#content");
+        const nCompDiv = document.createElement("div");
+        const compDiv = document.createElement("div");
+        const compH1 = document.createElement("h1");
+
+        contentDiv.replaceChildren();
+        
+        nCompDiv.id = "notCompleted";
+        nCompDiv.id = "completed";
+
+        compH1.textContent = "Completed Tasks";
+
+        compDiv.append(compH1);
+        contentDiv.append(nCompDiv);
+        contentDiv.append(compDiv);
+
+        // Update project tab class to change style
         const projectList = document.querySelectorAll(".project");
         console.log(projectList)
         projectList.forEach((elem) => {
@@ -33,9 +50,7 @@ const displayController = {
             }
         });
 
-        // Display tasks
-        const nCompDiv = document.querySelector("#notCompleted")
-        const compDiv = document.querySelector("#completed")
+        // Display project tasks with a few key properties
         for (let task of taskArray) {
             const taskDiv = document.createElement("div"); // need to track this after creation and associate it to task
             const taskCheck = document.createElement("input")
